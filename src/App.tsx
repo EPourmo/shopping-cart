6a28c0f8c4303ea33ab1197a8108308ac2eec200
EPourmo <94918200+EPourmo@users.noreply.github.com>
2023-01-01 22:38:18 +0100

    work on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import About from "./components/About";
-import Home from "./components/Home";
-import Store from "./components/Store";
+import NavBar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
 function App() {
   return (
-    <div>
+    <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
