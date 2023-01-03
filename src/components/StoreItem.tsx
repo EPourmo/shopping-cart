c23856607015860a5894cdba6247e47a8ab471ee
EPourmo <94918200+EPourmo@users.noreply.github.com>
2023-01-03 21:45:49 +0100

    work on cards style

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -7,8 +7,14 @@ type StoreItemProps = {
 
 export default function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
   return (
-    <>
-      <img src={imgUrl} alt={name} />
-    </>
+    <div className="h-80 rounded-md bg-white shadow-lg overflow-hidden">
+      <img src={imgUrl} alt={name} className="h-52 object-cover w-full" />
+      <div className="flex flex-col p-6">
+        <div className="flex flex-row items-baseline justify-between mb-4">
+          <h2 className="text-2xl">{name}</h2>
+          <p className="text-xl text-gray-400">{price}</p>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,14 +3,16 @@ import StoreItem from "../components/StoreItem";
 export default function Store() {
   return (
     <>
-      <h1>Store</h1>;
-      {storeItems.map((item) => {
-        return (
-          <div key={item.id}>
-            <StoreItem {...item} />;
-          </div>
-        );
-      })}
+      <h1>Store</h1>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-4">
+        {storeItems.map((item) => {
+          return (
+            <div key={item.id}>
+              <StoreItem {...item} />
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
