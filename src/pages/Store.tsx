3f587f4f6e1757b8de18cf5095ac4b1d32f7a205
EPourmo <94918200+EPourmo@users.noreply.github.com>
2023-01-03 21:47:48 +0100

    small css change

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,8 +3,8 @@ import StoreItem from "../components/StoreItem";
 export default function Store() {
   return (
     <>
-      <h1>Store</h1>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-4">
+      <h1 className="text-4xl m-8">Store</h1>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-8">
         {storeItems.map((item) => {
           return (
             <div key={item.id}>
