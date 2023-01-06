8d21ac916a42654402a7dcdc34392a37adbf7842
EPourmo <94918200+EPourmo@users.noreply.github.com>
2023-01-06 21:59:15 +0100

    create needed functions and const for sidebar cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import ShoppingSvg from "./ShoppingSvg";
 export default function NavBar() {
+  const { openCart, closeCart, cartQuantity } = useShoppingCart();
   return (
     <div className="bg-white shadow-sm sticky">
       <div className="flex items-center text-2xl p-4">
@@ -13,12 +15,17 @@ export default function NavBar() {
           </Link>
           <Link to={"/about"}>About</Link>
         </nav>
-        <button className="bg-sky-600 p-4 rounded-full hover:bg-sky-800 relative">
-          {<ShoppingSvg />}
-          <div className="absolute bg-red-600 text-white rounded-full p-1 h-6 w-6 text-base flex items-center justify-center right-0 ">
-            5
-          </div>
-        </button>
+        {cartQuantity > 0 && (
+          <button
+            className="bg-sky-600 p-4 rounded-full hover:bg-sky-800 relative"
+            onClick={openCart}
+          >
+            {<ShoppingSvg />}
+            <div className="absolute bg-red-600 text-white rounded-full p-1 h-6 w-6 text-base flex items-center justify-center right-0 ">
+              {cartQuantity}
+            </div>
+          </button>
+        )}
       </div>
     </div>
   );
