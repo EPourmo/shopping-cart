3e380065c27fe87cd05e186e09c7adf258ba929c
EPourmo <94918200+EPourmo@users.noreply.github.com>
2023-01-08 00:47:37 +0100

    work on shopping cart sidebar

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,41 @@
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import storeItem from "../data/items.json";
+import formatCurrency from "../utilities/formatCurrency";
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+export default function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItem.find((i) => i.id === id);
+  if (item == null) return null;
+
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={item.imgUrl} className=" h-20 w-32 object-cover mr-2" />
+          <div>
+            <p>
+              {item.name}{" "}
+              {quantity > 1 && (
+                <span className=" text-xs text-zinc-600">x{quantity}</span>
+              )}{" "}
+            </p>
+            <p>{formatCurrency(item.price)}</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <p className="mr-4">{formatCurrency(item.price * quantity)}</p>
+          <button
+            className="h-10 w-10 rounded-md border-2 hover:text-red-600 hover:border-red-600 "
+            onClick={() => removeFromCart(item.id)}
+          >
+            x
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,28 +1,46 @@
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import CartItem from "./CartItem";
+import StoreItem from "./StoreItem";
+import storeItems from "../data/items.json";
+import formatCurrency from "../utilities/formatCurrency";
 
 type ShoppingCartProps = {
   isOpen: boolean;
 };
 
 export default function ShoppingCart({ isOpen }: ShoppingCartProps) {
-  const { closeCart } = useShoppingCart();
+  const { closeCart, cartItems } = useShoppingCart();
   return (
-    <>
+    <div
+      className={`fixed top-0 w-10/12 sm:w-9/12 md:w-6/12 h-full right-0 z-50 bg-slate-50 shadow-xl  ease-in-out duration-300 ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      } p-4`}
+    >
       {isOpen && (
         <button
-          className="flex text-4xl text-black items-center cursor-pointer fixed right-10 top-3 z-50 m-0"
+          className="text-4xl text-black items-center cursor-pointer fixed right-10 top-3 z-50 m-0"
           onClick={closeCart}
         >
           x
         </button>
       )}
-      <div
-        className={`fixed w-3/5 top-0 h-full right-0 z-10 bg-slate-50 translate-x-10 shadow-xl flex justify-start ease-in-out duration-300 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        } p-4`}
-      >
-        <h2 className="text-4xl">Cart</h2>
+      <div>
+        <h2 className="text-4xl mb-6">Cart</h2>
+        <div className="flex gap-4 flex-col mb-4">
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </div>
+        <div className=" font-extrabold text-2xl text-right">
+          {`Total: ` +
+            formatCurrency(
+              cartItems.reduce((total, CartItem) => {
+                const item = storeItems.find((i) => i.id === CartItem.id);
+                return total + (item?.price || 0) * CartItem.quantity;
+              }, 0)
+            )}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
